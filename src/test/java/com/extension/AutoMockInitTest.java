b977608a135d297c18ae1e4b515d8c88608134e8
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2025-02-20 23:27:57 +0300

    add tests for auto initialization mock

---
+++ src/test/java/com/extension/AutoMockInitTest.java
@@ -0,0 +1,34 @@
+package com.extension;
+
+import com.domen.model.Book;
+import com.domen.model.Edition;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(MockitoExtension.class)
+public class AutoMockInitTest {
+    @Mock
+    private Edition mockEdition;
+
+    @Test
+    void testAddEdition() {
+        Book book = new Book.BookBuilder().setTitle("Test Book").setIntroduction("Test Intro").build();
+
+        book.addEdition(mockEdition);
+
+        assertTrue(book.getEditions().contains(mockEdition));
+    }
+
+    @Test
+    void testEditionConflict() {
+        Book book = new Book.BookBuilder().setTitle("Test Book").setIntroduction("Test Intro").build();
+        Edition edition1 = new Edition(1);
+        book.addEdition(edition1);
+        Edition edition2 = new Edition(1);
+
+        assertThrows(IllegalArgumentException.class, () -> book.addEdition(edition2));
+    }
+}
